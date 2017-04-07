3fb54b913b8cd2c2f8cac87789ad6c632d9b9577
juancarpio27 <juan.carpio01@gmail.com>
2017-04-06 19:17:57 -0500

    stores

--- app/controllers/admin/stores_controller.rb
+++ app/controllers/admin/stores_controller.rb
@@ -4,4 +4,49 @@ class Admin::StoresController < AdminController
     @stores = Store.all
   end
 
+  def show
+    @store = Store.find(params[:id])
+  end
+
+  def new
+    @store = Store.new
+  end
+
+  def create
+    @store = Store.new(store_params)
+    if @store.save
+      redirect_to admin_stores_path
+    else
+      redirect_to new_admin_store_path
+    end
+  end
+
+  def edit
+    @store = Store.find(params[:id])
+  end
+
+  def update
+    @store = Store.find(params[:id])
+    if @store.update(store_params)
+      redirect_to admin_stores_path
+    else
+      redirect_to edit_admin_store_path
+    end
+  end
+
+  def destroy
+    @store = Store.find(params[:id])
+    if @store.destroy
+      redirect_to admin_stores_path
+    else
+      redirect_to admin_stores_path
+    end
+  end
+
+  protected
+
+  def store_params
+    params.require(:store).permit(:name, :address, :closing_hour, :opening_hour, :phone)
+  end
+
 end
--- app/models/store.rb
+++ app/models/store.rb
@@ -1,2 +1,9 @@
 class Store < ApplicationRecord
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :opening_hour, presence: true
+  validates :closing_hour, presence: true
+  validates :phone, presence: true
+
 end
