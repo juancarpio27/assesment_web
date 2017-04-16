28c6c592405614a1b0287701e8a4e36511fb53a1
juancarpio27 <juan.carpio01@gmail.com>
2017-04-16 10:03:14 -0500

    stores api

--- app/controllers/api/stores_controller.rb
+++ app/controllers/api/stores_controller.rb
@@ -2,12 +2,12 @@ class Api::StoresController < ApiController
 
   def index
     @stores = Store.all
-    render json: @stores.as_json(methods: [:products]), status: 200
+    render json: @stores.as_json
   end
 
-  def over_price
-    @products = Store.find(params[:id]).products.where('price > ?',params[:price])
-    render json: @products.as_json
+  def show
+    @store = Store.find(params[:id])
+    render json @store.as_json
   end
 
 end
